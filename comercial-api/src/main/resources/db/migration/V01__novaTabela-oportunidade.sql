7398f35072468bd839fd8b68ee18eba6a2209a21
RaifePaiva <47899204+RaifePaiva@users.noreply.github.com>
2020-02-19 01:39:43 -0300

    Add files via upload

---
+++ comercial-api/src/main/resources/db/migration/V01__novaTabela-oportunidade.sql
@@ -0,0 +1,10 @@
+create table oportunidade (
+	
+	id bigint auto_increment not null,
+	nome_prospecto varchar(100) not null,
+	descricao varchar(200) not null,
+	valor decimal (10,2),
+	
+	primary key(id)
+
+);
